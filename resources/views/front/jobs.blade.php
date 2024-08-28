4d5bbd4ee0711d36babfb2d51116b8a764c8bb9a
Hauser <hauserkun@gmail.com>
2024-08-29 02:04:40 +0545

    Update

--- resources/views/front/jobs.blade.php
+++ resources/views/front/jobs.blade.php
@@ -65,17 +65,17 @@
                                 <h2>Experience</h2>
                                 <select name="experience" id="experience" class="form-control">
                                     <option value="">Select Experience</option>
-                                    <option value="">1 Year</option>
-                                    <option value="">2 Years</option>
-                                    <option value="">3 Years</option>
-                                    <option value="">4 Years</option>
-                                    <option value="">5 Years</option>
-                                    <option value="">6 Years</option>
-                                    <option value="">7 Years</option>
-                                    <option value="">8 Years</option>
-                                    <option value="">9 Years</option>
-                                    <option value="">10 Years</option>
-                                    <option value="">10+ Years</option>
+                                    <option value="1">1 Year</option>
+                                    <option value="2">2 Years</option>
+                                    <option value="3">3 Years</option>
+                                    <option value="4">4 Years</option>
+                                    <option value="5">5 Years</option>
+                                    <option value="6">6 Years</option>
+                                    <option value="7">7 Years</option>
+                                    <option value="8">8 Years</option>
+                                    <option value="9">9 Years</option>
+                                    <option value="10">10 Years</option>
+                                    <option value="10_plus">10+ Years</option>
                                 </select>
                             </div>
 
